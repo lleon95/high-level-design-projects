837b930bf754ea37f2e7c6fdb850d9e7cf3427d5
Luis Leon <lleon95@gmail.com>
2019-06-30 12:19:07 -0600

    Add testbench for VGA encoder

--- src/vga-encoder/vga-encoder-tbb.cpp
+++ src/vga-encoder/vga-encoder-tbb.cpp
@@ -1,67 +1,117 @@
 #include "systemc.h"
-#include "image-processor.cpp"
+
+#include "vga-encoder.hpp"
+#include "vga-encoder.cpp"
+
+#define RUNTIME 17000000000 /* 17ms */
+#define RUNTIME_STEP 10000 /* 10ns */
+#define CHANNEL_WIDTH 4
+
+#define COLOR_COL_WIDTH 20
+
+sc_uint<12> pixel_compute ();
 
 int sc_main (int argc, char* argv[]) {
-  sc_signal<bool>   clock;
-  sc_signal<bool>   reset;
-  sc_signal<bool>   enable;
-  sc_signal<sc_uint<4> > counter_out;
-  int i = 0;
-  // Connect the DUT
-  first_counter counter("COUNTER");
-    counter.clock(clock);
-    counter.reset(reset);
-    counter.enable(enable);
-    counter.counter_out(counter_out);
+  /* Module in/control */
+  sc_signal<sc_uint<12> > pixel_in;
+  sc_signal<bool>  pixel_unqueue;
+  sc_signal<sc_uint<19> >  pixel_counter;
 
-  sc_start(1,SC_NS);
+  /* Module out */
+  sc_signal<bool>  h_sync;
+  sc_signal<bool>  v_sync;
+  sc_signal<sc_uint<4> > red_channel;
+  sc_signal<sc_uint<4> > green_channel;
+  sc_signal<sc_uint<4> > blue_channel;
 
-  // Open VCD file
-  sc_trace_file *wf = sc_create_vcd_trace_file("counter");
-  // Dump the desired signals
-  sc_trace(wf, clock, "clock");
-  sc_trace(wf, reset, "reset");
-  sc_trace(wf, enable, "enable");
-  sc_trace(wf, counter_out, "count");
+  /* Connect the DUT */
+  vga_encoder video_out("video_out");
+    /* Input/Control */
+    video_out.pixel_in(pixel_in);
+    video_out.pixel_unqueue(pixel_unqueue);
+    video_out.pixel_counter(pixel_counter);
+    /* Output */
+    video_out.h_sync(h_sync);
+    video_out.v_sync(v_sync);
+    video_out.red_channel(red_channel);
+    video_out.green_channel(green_channel);
+    video_out.blue_channel(blue_channel);
 
-  // Initialize all variables
-  reset = 0;       // initial value of reset
-  enable = 0;      // initial value of enable
-  for (i=0;i<5;i++) {
-    clock = 0; 
-    sc_start(1,SC_NS);
-    clock = 1; 
-    sc_start(1,SC_NS);
-  }
-  reset = 1;    // Assert the reset
-  cout << "@" << sc_time_stamp() <<" Asserting reset\n" << endl;
-  for (i=0;i<10;i++) {
-    clock = 0; 
-    sc_start(1,SC_NS);
-    clock = 1; 
-    sc_start(1,SC_NS);
-  }
-  reset = 0;    // De-assert the reset
-  cout << "@" << sc_time_stamp() <<" De-Asserting reset\n" << endl;
-  for (i=0;i<5;i++) {
-    clock = 0; 
-    sc_start(1,SC_NS);
-    clock = 1; 
-    sc_start(1,SC_NS);
-  }
-  cout << "@" << sc_time_stamp() <<" Asserting Enable\n" << endl;
-  enable = 1;  // Assert enable
-  for (i=0;i<20;i++) {
-    clock = 0; 
-    sc_start(1,SC_NS);
-    clock = 1; 
-    sc_start(1,SC_NS);
-  }
-  cout << "@" << sc_time_stamp() <<" De-Asserting Enable\n" << endl;
-  enable = 0; // De-assert enable
+  /* Open VCD file */
+  sc_trace_file *wf = sc_create_vcd_trace_file("video_encoder");
+  /* Dump the desired signals */
+  sc_trace(wf, pixel_in, "pixel_in");
+  sc_trace(wf, pixel_unqueue, "pixel_unqueue");
+  sc_trace(wf, pixel_counter, "pixel_counter");
+  sc_trace(wf, h_sync, "h_sync");
+  sc_trace(wf, v_sync, "v_sync");
+  sc_trace(wf, red_channel, "red_channel");
+  sc_trace(wf, green_channel, "green_channel");
+  sc_trace(wf, blue_channel, "blue_channel");
+  
+  /* Initialise */
+  sc_start(0,SC_NS);
+  video_out.reset();
+  cout << "@" << sc_time_stamp() <<" Starting simulation\n"<< endl;
 
+  /* Print signals */
+  int runtime = 0; 
+  int jump_time = RUNTIME_STEP;
+  for (runtime = 0; runtime < RUNTIME; runtime += jump_time){
+    /* Simulation step */
+    sc_start(jump_time,SC_PS);
+    /* Logic */
+    if(pixel_unqueue.read()){
+      jump_time = DELAY_SEND_PIXELS;
+      cout << "@" << sc_time_stamp() <<" New pixel request... " << endl;
+      pixel_in.write(pixel_compute ());
+      video_out.write();
+      cout << "@" << sc_time_stamp() <<" Pixel written... " << endl;
+      video_out.read();    
+      cout << "@" << sc_time_stamp() <<" Current pixel position: " 
+          << pixel_counter.read() << endl;
+    }
+    else {
+      jump_time = RUNTIME_STEP;
+    }
+  }
+
+  /* Terminate */
   cout << "@" << sc_time_stamp() <<" Terminating simulation\n" << endl;
   sc_close_vcd_trace_file(wf);
-  return 0;// Terminate simulation
-
+  return 0;
 }
+
+sc_uint<12> pixel_compute (){
+  sc_uint<12> pixel;
+  static sc_uint<10>	col; 
+  static sc_uint<9>	row; 
+
+  static sc_uint<4> red = 0x0;
+  static sc_uint<4> green = 0X5;
+  static sc_uint<4> blue = 0xA;
+
+  /* Counters logic */
+  if(col < COLS){
+    col++;
+  } else {
+    row++;
+    col = 0;
+  }
+  if(row == ROWS){
+    row = 0;
+  }
+
+  /* Image builder */
+  if(col % COLOR_COL_WIDTH == 0){
+    red += 0x5;
+    green += 0xA;
+    blue += 0xF;
+  }
+
+  pixel.range(CHANNEL_WIDTH - 1, 0) = blue;
+  pixel.range(2*CHANNEL_WIDTH - 1, CHANNEL_WIDTH) = green;
+  pixel.range(3*CHANNEL_WIDTH - 1, 2*CHANNEL_WIDTH) = red;
+  return pixel;
+}
+
