e86a29c140ba9600d8dde6c437d7fb079d8bb9c3
Emmanuel Madrigal <emma.madri97@gmail.com>
2019-07-13 16:07:14 -0600

    Move grayscaler to operate as a CPU device

--- src/grayscaler/grayscaler-tbb.cpp
+++ src/grayscaler/grayscaler-tbb.cpp
@@ -9,14 +9,16 @@
 #define TEST_ITERATIONS 20
 
 /* Get the exact expected value */
-double
+int
 rgb12_to_gray(int pixel)
 {
-    int r = (pixel >> 8) & 0xF;
-    int g = (pixel >> 4) & 0xF;
-    int b = pixel & 0xF;
+    int r = ((pixel & 0xF00) >> 8) * 0.3;
+    int g = ((pixel & 0x0F0) >> 4) * 0.59;
+    int b = ((pixel & 0x00F) >> 0) * 0.11;
 
-    return r * 0.3 + g * 0.59 + b * 0.11;
+    int gray = r + g + b;
+
+    return (gray << (CHANNEL_WIDTH * 2)) + (gray << (CHANNEL_WIDTH * 1)) + gray;
 }
 
 int
@@ -26,45 +28,54 @@ sc_main (int argc, char* argv[])
     srand (time(NULL));
 
     sc_signal<bool>   enable;
-    sc_signal<sc_uint<PIXEL_IN_WIDTH>> pix_in;
-    sc_signal<sc_uint<PIXEL_OUT_WIDTH>> pix_out;
+    sc_signal<sc_uint<PIXEL_WIDTH>> pix_data;
+    sc_signal<sc_uint<ADDRESS_WIDTH>> pix_address;
 
     int i = 0;
     double total_error = 0;
+
     /* Connect the DUT */
     grayscaler processor("GRAYSCALER");
     processor.enable(enable);
-    processor.pix_out(pix_out);
-    processor.pix_in(pix_in);
+    processor.pix_data(pix_data);
+    processor.pix_address(pix_address);
 
     sc_start(1,SC_NS);
 
     /* Open VCD file */
     sc_trace_file *wf = sc_create_vcd_trace_file("grayscaler");
+
     /* Dump the desired signals */
     sc_trace(wf, enable, "enable");
-    sc_trace(wf, pix_in, "pix_in");
-    sc_trace(wf, pix_in, "pix_in");
-    sc_trace(wf, pix_out, "pix_out");
+    sc_trace(wf, pix_data, "pix_data");
+    sc_trace(wf, pix_address, "pix_address");
 
     /* Initialize all variables */
-    cout << "@" << sc_time_stamp() <<" Asserting Enable\n" << endl;
-    enable = 1;  /* Assert enable */
     for (i=0; i<TEST_ITERATIONS; i++) {
         /* Generate random number */
-        random_pixel = rand() % ( 1 << PIXEL_IN_WIDTH );
-        pix_in.write(random_pixel);
+        random_pixel = rand() % ( 1 << PIXEL_WIDTH );
+
+        pix_data.write(random_pixel);
         sc_start(1,SC_NS);
-        total_error += abs(pix_out.read() - rgb12_to_gray(random_pixel));
-        cout << "input pixel: " << random_pixel << "\tsystemC output = " <<
-             pix_out.read() << "\texpected output = " << rgb12_to_gray(random_pixel) << endl;
+
+        /* Start frame */
+        enable.write(1);
+        sc_start(1,SC_NS);
+
+        /*
+        * Test last pixel, since this is PV all pixels are inmediatly
+               * available
+               */
+        total_error += abs((int) pix_data.read() - rgb12_to_gray(random_pixel));
+        printf("input pixel: %x\tsystemC output = %x\tpix_data.read() = %x\n",
+               random_pixel, (int)pix_data.read(),  rgb12_to_gray(random_pixel));
+
+
+        enable.write(0);
+        sc_start(1,SC_NS);
     }
 
     cout << "Average Error: " << total_error / TEST_ITERATIONS << endl;
-
-
-    cout << "@" << sc_time_stamp() <<" De-Asserting Enable\n" << endl;
-    enable = 0; /* De-assert enable */
 
 
     cout << "@" << sc_time_stamp() <<" Terminating simulation\n" << endl;
--- src/grayscaler/grayscaler.cpp
+++ src/grayscaler/grayscaler.cpp
@@ -2,39 +2,81 @@
 
 #define WIDTH 640
 #define HEIGHT 480
-#define PIXEL_IN_WIDTH 12
-#define PIXEL_OUT_WIDTH 8
+#define PIXEL_WIDTH 12
 #define CHANNEL_WIDTH 4
+
+#define ADDRESS_WIDTH 20
+#define COLOR_IMAGE_START 0x0
+#define GRAY_IMAGE_START 0x4b000
+
+#define INTERRUPT_DELAY 0
 
 SC_MODULE (grayscaler)
 {
     sc_in<bool> enable;
-    sc_in<sc_uint<PIXEL_IN_WIDTH> > pix_in;
-    sc_out<sc_uint<PIXEL_OUT_WIDTH> > pix_out;
+    sc_inout<sc_uint<PIXEL_WIDTH> > pix_data;
+    sc_out<sc_uint<ADDRESS_WIDTH> > pix_address;
 
-    /*------------Local Variables------------------------- */
-    sc_uint<4> r;
-    sc_uint<4> g;
-    sc_uint<4> b;
+    sc_event _frame_start;
 
-    /*------------Code Starts Here-------------------------*/
+    /* Local Variables */
+    sc_uint<PIXEL_WIDTH> current_pixel;
+    sc_uint<2*CHANNEL_WIDTH> r;
+    sc_uint<2*CHANNEL_WIDTH> g;
+    sc_uint<2*CHANNEL_WIDTH> b;
+    sc_uint<CHANNEL_WIDTH> gray;
+
+    /* Interrupt Handler */
+    void frame_start() {
+        _frame_start.notify(INTERRUPT_DELAY, SC_NS);
+    }
+
+
     void convert_to_grayscale () {
-        if(enable.read() == 1) {
-            sc_uint<CHANNEL_WIDTH> r = pix_in.read().range(3*CHANNEL_WIDTH - 1,
-                                       2*CHANNEL_WIDTH );
-            sc_uint<CHANNEL_WIDTH> g = pix_in.read().range(2*CHANNEL_WIDTH - 1,
-                                       CHANNEL_WIDTH );
-            sc_uint<CHANNEL_WIDTH> b = pix_in.read().range(CHANNEL_WIDTH - 1, 0);
+        while(true) {
+            wait(_frame_start);
+            for(int i = 0; i < HEIGHT; i++) {
+                for(int j = 0; j < WIDTH; j++) {
+                    /* Copy value from memory */
+                    pix_address = COLOR_IMAGE_START + j + i * WIDTH;
 
-            pix_out = r * 0.3 + g * 0.59 + b * 0.11;
+                    current_pixel = pix_data.read();
 
+                    /* Get component values from memory */
+
+                    r = current_pixel.range(3*CHANNEL_WIDTH - 1,
+                                            2*CHANNEL_WIDTH) * 0.3;
+                    g = current_pixel.range(2*CHANNEL_WIDTH - 1,
+                                            1*CHANNEL_WIDTH) * 0.59;
+                    b = current_pixel.range(1*CHANNEL_WIDTH - 1,
+                                            0) * 0.11;
+
+                    /* Get grayscale value */
+                    gray =  r + g + b;
+
+                    /* Get values to output pixel */
+                    current_pixel.range(3*CHANNEL_WIDTH - 1,
+                                        2*CHANNEL_WIDTH) = gray;
+                    current_pixel.range(2*CHANNEL_WIDTH - 1,
+                                        1*CHANNEL_WIDTH) = gray;
+                    current_pixel.range(1*CHANNEL_WIDTH - 1,
+                                        0) = gray;
+
+                    /* Write new value back to memory  */
+                    pix_address = GRAY_IMAGE_START + j + i * WIDTH;
+                    pix_data.write(current_pixel);
+
+                }
+            }
         }
     }
 
     SC_CTOR(grayscaler) {
         cout<<"Executing new"<<endl;
-        SC_METHOD(convert_to_grayscale);
-        sensitive << pix_in;
+        SC_THREAD(convert_to_grayscale);
+
+        SC_METHOD(frame_start);
+        sensitive << enable;
     } /* End of Constructor */
 
 }; /* End of Module counter */
