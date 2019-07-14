8951b0de8c108850acf2a2f00723ae021ec2445b
eliecer9000 <eliecer9000@gmail.com>
2019-07-13 19:12:02 -0700

    File deleted as the makefile replaces this one.

--- src/compile_run_vga_decoder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo 'Compiling *.c *cpp files'
-rm -rf vga_decoder.o
-export SYSTEMC_HOME=/usr/local/systemc-2.3.3/
-export LD_LIBRARY_PATH=$SYSTEMC_HOME/lib-linux64
-g++ -I$SYSTEMC_HOME/include -L$SYSTEMC_HOME/lib-linux64 vga_decoder.cpp vga_decoder_tb.cpp  -lsystemc -lm -o vga_decoder.o
-echo 'Simulation Started'
-./vga_decoder.o
-echo 'Simulation Ended'
