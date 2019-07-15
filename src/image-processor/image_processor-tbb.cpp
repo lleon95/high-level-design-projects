01a195b6b70af629d00e1fcc6071f48ce034198c
Emmanuel Madrigal <emmanuel.madrigal@ridgerun.com>
2019-07-15 09:25:02 -0600

    Refactor processor logic

--- src/image-processor/image_processor-tbb.cpp
+++ src/image-processor/image_processor-tbb.cpp
@@ -4,11 +4,24 @@
 #include <systemc.h>
 #include <time.h>
 
-#include "image_processor.cpp"
-
+#include "image_processor.hpp"
 #define TEST_ITERATIONS 10
 
-/* Get the exact expected value */
+/* Definition of Sobel filter in horizontal direction */
+const int h_weights[3][3] = {
+    { -1,  0,  1 },
+    { -2,  0,  2 },
+    { -1,  0,  1 }
+};
+
+/* Definition of Sobel filter in vertical direction */
+const int v_weights[3][3] = {
+    { -1,  -2, -1 },
+    { 0,  0,  0 },
+    { 1,  2,  1 }
+};
+
+
 int
 rgb12_to_gray(int pixel)
 {
@@ -17,9 +30,35 @@ rgb12_to_gray(int pixel)
     int b = ((pixel & 0x00F) >> 0) * 0.11;
 
     int gray = r + g + b;
+    gray = gray  % ( 1 << CHANNEL_WIDTH );
 
-    return (gray << (CHANNEL_WIDTH * 2)) + (gray << (CHANNEL_WIDTH * 1)) + gray;
+    return gray;
 }
+
+int
+rgb12_to_sobel(int pixels[9])
+{
+    int h_value = 0;
+    int v_value = 0;
+    int result;
+
+    for (int i = 0; i < 3; i++) {
+        for( int j = 0; j < 3; j++) {
+            h_value += h_weights[i][j] * rgb12_to_gray(pixels[i* 3 + j]);
+            v_value += v_weights[i][j] * rgb12_to_gray(pixels[i* 3 + j]);
+        }
+    }
+
+    /* Get sobel value */
+    result =  sqrt(h_value*h_value + v_value*v_value);
+    result = result  % ( 1 << CHANNEL_WIDTH );
+
+
+    return (result << (CHANNEL_WIDTH * 2)) + (result << (CHANNEL_WIDTH * 1)) + result;
+}
+
+
+
 
 int
 sc_main (int argc, char* argv[])
@@ -27,10 +66,11 @@ sc_main (int argc, char* argv[])
     int random_pixel = 0;
     srand (time(NULL));
 
+    int test_pixels[9];
     sc_signal<bool>   enable;
-    sc_signal<sc_uint<PIXEL_WIDTH>> pix_data;
-    sc_signal<sc_uint<PIXEL_WIDTH>> pix_data_out;
-    sc_signal<sc_uint<ADDRESS_WIDTH>> pix_address;
+    sc_signal<sc_uint<PIXEL_WIDTH> > pixels[9];
+
+    sc_signal<sc_uint<PIXEL_WIDTH> > pix_out;
 
     int i = 0;
     double total_error = 0;
@@ -38,9 +78,11 @@ sc_main (int argc, char* argv[])
     /* Connect the DUT */
     image_processor processor("GRAYSCALER");
     processor.enable(enable);
-    processor.pix_data(pix_data);
-    processor.pix_data_out(pix_data_out);
-    processor.pix_address(pix_address);
+    for(int i = 0; i < 9; i++) {
+        processor.in_pixels[i](pixels[i]);
+    }
+
+    processor.pix_out(pix_out);
 
     sc_start(1,SC_NS);
 
@@ -49,15 +91,26 @@ sc_main (int argc, char* argv[])
 
     /* Dump the desired signals */
     sc_trace(wf, enable, "enable");
-    sc_trace(wf, pix_data, "pix_data");
-    sc_trace(wf, pix_address, "pix_address");
+    sc_trace(wf, pix_out, "pix_out");
+    sc_trace(wf, pixels[0], "top_left");
+    sc_trace(wf, pixels[1], "top_center");
+    sc_trace(wf, pixels[2], "top_right");
+    sc_trace(wf, pixels[3], "center_left");
+    sc_trace(wf, pixels[4], "center");
+    sc_trace(wf, pixels[5], "center_right");
+    sc_trace(wf, pixels[6], "bottom_left");
+    sc_trace(wf, pixels[7], "bottom_center");
+    sc_trace(wf, pixels[8], "bottom_right");
 
     /* Initialize all variables */
     for (i=0; i<TEST_ITERATIONS; i++) {
         /* Generate random number */
-        random_pixel = rand() % ( 1 << PIXEL_WIDTH );
+        for (int i = 0; i < 9; i++) {
+            random_pixel = rand() % ( 1 << PIXEL_WIDTH );
+	    test_pixels[i] = random_pixel;
+            pixels[i].write(random_pixel);
+        }
 
-        pix_data.write(random_pixel);
         sc_start(1,SC_NS);
 
         /* Start frame */
@@ -68,9 +121,9 @@ sc_main (int argc, char* argv[])
         * Test last pixel, since this is PV all pixels are inmediatly
                * available
                */
-        total_error += abs((int) pix_data.read() - rgb12_to_gray(random_pixel));
+        total_error += abs((int) pix_out.read() - rgb12_to_sobel(test_pixels));
         printf("input pixel: %x\tsystemC output = %x\texpected output = %x\n",
-               random_pixel, (int)pix_data_out.read(),  rgb12_to_gray(random_pixel));
+               random_pixel, (int)pix_out.read(),  rgb12_to_sobel(test_pixels));
 
 
         enable.write(0);
--- src/image-processor/image_processor.cpp
+++ src/image-processor/image_processor.cpp
@@ -1,22 +1,6 @@
-#include "systemc.h"
-
 #include <math.h>
 
-//#define WIDTH 640
-//#define HEIGHT 480
-
-#define WIDTH 4
-#define HEIGHT 4
-
-
-#define PIXEL_WIDTH 12
-#define CHANNEL_WIDTH 4
-
-#define ADDRESS_WIDTH 20
-#define INPUT_IMAGE_START 0x0
-#define OUTPUT_IMAGE_START 0x4b000
-
-#define INTERRUPT_DELAY 0
+#include "image_processor.hpp"
 
 /* Definition of Sobel filter in horizontal direction */
 const sc_int<CHANNEL_WIDTH+1> h_weights[3][3] = {
@@ -32,129 +16,81 @@ const sc_int<CHANNEL_WIDTH+1> v_weights[3][3] = {
     { 1,  2,  1 }
 };
 
-
-SC_MODULE (image_processor)
+/* Interrupt Handler */
+void
+image_processor::frame_start()
 {
-    sc_in<bool> enable;
-    sc_inout<sc_uint<PIXEL_WIDTH> > pix_data;
-    sc_inout<sc_uint<PIXEL_WIDTH> > pix_data_out;
-    
-    sc_out<sc_uint<ADDRESS_WIDTH> > pix_address;
+    _frame_start.notify(INTERRUPT_DELAY, SC_NS);
+}
 
-    sc_event _frame_start;
+static sc_uint<CHANNEL_WIDTH>
+apply_sobel (sc_uint<CHANNEL_WIDTH> pixels[3][3])
+{
+    sc_int<2*CHANNEL_WIDTH> h_value = 0;
+    sc_int<2*CHANNEL_WIDTH> v_value = 0;
 
-    /* Local Variables */
-    sc_uint<PIXEL_WIDTH> current_pixel;
+    /* Get horizontal values */
+    for (int i = 0; i < 3; i++) {
+        for( int j = 0; j < 3; j++) {
+            h_value += h_weights[i][j] * pixels[i][j];
+        }
+    }
 
+    /* Get vertical values */
+    for (int i = 0; i < 3; i++) {
+        for( int j = 0; j < 3; j++) {
+            v_value += v_weights[i][j] * pixels[i][j];
+        }
+    }
+
+    /* Get sobel value */
+    return sqrt(h_value*h_value + v_value*v_value);
+}
+
+static sc_uint<CHANNEL_WIDTH>
+convert_to_grayscale (sc_uint<PIXEL_WIDTH> pixel)
+{
+    sc_uint<CHANNEL_WIDTH> gray;
     sc_uint<CHANNEL_WIDTH> r;
     sc_uint<CHANNEL_WIDTH> g;
     sc_uint<CHANNEL_WIDTH> b;
-    sc_uint<CHANNEL_WIDTH> gray;
+    
+    /* Get component values from memory */
+    r = pixel.range(3*CHANNEL_WIDTH - 1,
+                    2*CHANNEL_WIDTH) * 0.3;
+    g = pixel.range(2*CHANNEL_WIDTH - 1,
+                    1*CHANNEL_WIDTH) * 0.59;
+    b = pixel.range(1*CHANNEL_WIDTH - 1,
+                    0) * 0.11;
 
-    sc_uint<CHANNEL_WIDTH> pixels[3][3];
+    /* Get grayscale value */
+    gray =  r + g + b;
 
-    /* Interrupt Handler */
-    void frame_start() {
-        _frame_start.notify(INTERRUPT_DELAY, SC_NS);
-    }
+    return gray;
+}
 
-    sc_uint<CHANNEL_WIDTH> apply_sobel () {
-        sc_int<2*CHANNEL_WIDTH> h_value = 0;
-        sc_int<2*CHANNEL_WIDTH> v_value = 0;
+void
+image_processor::process()
+{
+    while(true) {
+        wait(_frame_start);
 
-        /* Get horizontal values */
+        /* Get grayscale value for all pixels */
         for (int i = 0; i < 3; i++) {
             for( int j = 0; j < 3; j++) {
-                h_value += h_weights[i][j] * pixels[i][j];
+                pixels[i][j] = convert_to_grayscale (in_pixels[i + j*3]);
             }
         }
 
-        /* Get vertical values */
-        for (int i = 0; i < 3; i++) {
-            for( int j = 0; j < 3; j++) {
-                v_value += v_weights[i][j] * pixels[i][j];
-            }
-        }
+        /* Apply grayscale */
+        gray = apply_sobel(pixels);
 
-        /* Get sobel value */
-        return sqrt(h_value*h_value + v_value*v_value);
+        /* Get values to output pixel */
+        current_pixel.range(3*CHANNEL_WIDTH - 1, 2*CHANNEL_WIDTH) = gray;
+        current_pixel.range(2*CHANNEL_WIDTH - 1, 1*CHANNEL_WIDTH) = gray;
+        current_pixel.range(1*CHANNEL_WIDTH - 1, 0) = gray;
 
+        /* Write value back*/
+        pix_out.write(current_pixel);
     }
-
-    sc_uint<CHANNEL_WIDTH> convert_to_grayscale (sc_uint<PIXEL_WIDTH> pixel) {
-        /* Get component values from memory */
-        r = pixel.range(3*CHANNEL_WIDTH - 1,
-                        2*CHANNEL_WIDTH) * 0.3;
-        g = pixel.range(2*CHANNEL_WIDTH - 1,
-                        1*CHANNEL_WIDTH) * 0.59;
-        b = pixel.range(1*CHANNEL_WIDTH - 1,
-                         0) * 0.11;
-
-        /* Get grayscale value */
-        gray =  r + g + b;
-
-        return gray;
-    }
-
-    void process() {
-        while(true) {
-            wait(_frame_start);
-
-	    /* Operate over all pixels */
-            for(int i = 0; i < HEIGHT; i++) {
-                for(int j = 0; j < WIDTH; j++) {
-		  
-                    /* If this is the first pixel copy the values to the interal buffers */
-                    if( (i == 0) && (j == 0) ) {
-                        for (int k = 0; k < 3; k++) {
-                            for( int l = 0; l < 3; l++) {
-                                pix_address = INPUT_IMAGE_START + j + l  + (i +k ) * WIDTH;
-                                pixels[k][l] = convert_to_grayscale (pix_data.read());
-                            }
-                        }
-                    } else {
-                        /* Update the internal values from the old values */
-                        for (int k = 0; k < 3; k++) {
-                            for( int l = 0; l < 2; l++) {
-			      pixels[k][l] = (int)pixels[k][l+1];
-                            }
-                        }
-
-                        /* Get new values from the memory */
-                        for (int k = 0; k < 3; k++) {
-			  pix_address = INPUT_IMAGE_START + (j + 2) + (i+k) * WIDTH;
-                            pixels[k][2] = convert_to_grayscale (pix_data.read());
-                        }
-                    }
-
-                    /* Apply sobel if not on the border */
-                    if( (i != 0) && ( i != (HEIGHT - 1)) && (j != 0) && (j != (WIDTH -1))) {
-                        gray = apply_sobel();
-                    }
-                    /* Otherwise use passthrough */
-                    else {
-		      gray = pixels[1][1];
-                    }
-
-		    /* Get values to output pixel */
-		    current_pixel.range(3*CHANNEL_WIDTH - 1, 2*CHANNEL_WIDTH) = gray;
-		    current_pixel.range(2*CHANNEL_WIDTH - 1, 1*CHANNEL_WIDTH) = gray;
-		    current_pixel.range(1*CHANNEL_WIDTH - 1, 0) = gray;
-
-                    /* Write value back*/
-                    pix_address = OUTPUT_IMAGE_START + j + i * WIDTH;
-                    pix_data_out.write(current_pixel);
-                }
-            }
-        }
-    }
-
-    SC_CTOR(image_processor) {
-        cout<<"Executing new"<<endl;
-        SC_THREAD(process);
-
-        SC_METHOD(frame_start);
-        sensitive << enable;
-    } /* End of Constructor */
-
-}; /* End of Module counter */
+}
