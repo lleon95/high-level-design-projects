d0393a0356f36c3ba20c01c3e29e5fd00a0159ef
lleon <lleon95@gmail.com>
2019-07-27 09:47:59 -0600

    Add router

--- src/dummy-router/dummy-node.cpp
+++ src/dummy-router/dummy-node.cpp
@@ -26,15 +26,23 @@ SC_MODULE(Node)
   }
 
   void thread_process(){
+
+    /* Put your tasks here */
     target->module_address = addr;
     for(int i = 0; i < NODES; i++){
       wait(sc_time(BUS_DELAY, SC_NS));
       initiator->write(i, 16 * i + 1, rand() % 2);
     }
+    
   }
 
   void reading_process() {
     while(true) {
+
+
+      /* Put your reception logic here */
+
+
       wait(*(incoming_notification));
       bool transfer_next = target->transfer_package;
       bool command = target->command;
@@ -58,14 +66,97 @@ SC_MODULE(Node)
   }
 };
 
+SC_MODULE(Router)
+{
+  Initiator *initiator_node;
+  Initiator *initiator_ring;
+  Target    *target_node;
+  Target    *target_ring;
+
+  Node      *dummy_node;
+
+  int addr;
+
+  sc_event * incoming_notification_node;
+  sc_event * incoming_notification_ring;
+
+  SC_CTOR(Router)
+  {
+    // Instantiate components
+    initiator_node = new Initiator("initiator_node");
+    initiator_ring = new Initiator("initiator_ring");
+    target_node    = new Target   ("target_node");
+    target_ring    = new Target   ("target_ring");
+    dummy_node     = new Node     ("dummy_node");
+
+    target_node->module_address = addr;
+    target_ring->module_address = addr;
+
+    incoming_notification_node = &(target_node->new_package);
+    incoming_notification_ring = &(target_ring->new_package);
+
+    /* Interconnect internals */
+    dummy_node->addr = 0; /* Node interconnected to router */
+    dummy_node->initiator->socket.bind(target_node->socket);
+    initiator_node->socket.bind(dummy_node->target->socket);
+
+    // SC_THREAD(thread_process); 
+    SC_THREAD(reading_process_node);
+    SC_THREAD(reading_process_ring);
+
+  }
+
+  void reading_process_node() {
+    while(true) {
+      wait(*(incoming_notification_node));
+      bool transfer_next = target_node->transfer_package;
+      bool command = target_node->command;
+      unsigned short destination = target_node->destination_address;
+      unsigned short data = target_node->incoming_buffer;
+      wait(sc_time(BUS_DELAY, SC_NS));
+      
+      cout << "Node-> Destination address: " <<  destination
+      << " Me: " << addr << " Action - Transfer: "
+      << transfer_next << " Data: "
+      << data << " Command: " << command << endl;
+
+      /* Transfer to the next */
+      initiator_ring->write(destination, data, command);
+      cout << "Retransmitted to: " << addr + 1 << endl;
+    }
+  }
+
+  void reading_process_ring() {
+    while(true) {
+      wait(*(incoming_notification_ring));
+      bool command = target_ring->command;
+      unsigned short destination = target_ring->destination_address;
+      unsigned short data = target_ring->incoming_buffer;
+      wait(sc_time(BUS_DELAY, SC_NS));
+      
+      cout << "Ring-> Destination address: " <<  destination
+      << " Me: " << addr << " Data: "
+      << data << " Command: " << command << endl;
+
+      /* Transfer to the next */
+      if(addr == destination) {
+        initiator_ring->write(destination, data, command);
+        cout << "Retransmitted to: " << addr + 1 << endl;
+      } else {
+        initiator_node->write(0, data, command); /* 0 is the connected node */
+        cout << "Received by: " << addr << endl;
+      }
+    }
+  }
+};
 
 int sc_main(int argc, char* argv[])
 {
-  Node node1("node1");
-  Node node2("node2");
-  Node node3("node3");
-  Node node4("node4");
-  Node node5("node5");
+  Router node1("node1");
+  Router node2("node2");
+  Router node3("node3");
+  Router node4("node4");
+  Router node5("node5");
 
 
   /* Address assignment */
@@ -76,11 +167,11 @@ int sc_main(int argc, char* argv[])
   node5.addr = 4;
 
   /* Binding - Ring */
-  node1.initiator->socket.bind(node2.target->socket);
-  node2.initiator->socket.bind(node3.target->socket);
-  node3.initiator->socket.bind(node4.target->socket);
-  node4.initiator->socket.bind(node5.target->socket);
-  node5.initiator->socket.bind(node1.target->socket);
+  node1.initiator_ring->socket.bind(node2.target_ring->socket);
+  node2.initiator_ring->socket.bind(node3.target_ring->socket);
+  node3.initiator_ring->socket.bind(node4.target_ring->socket);
+  node4.initiator_ring->socket.bind(node5.target_ring->socket);
+  node5.initiator_ring->socket.bind(node1.target_ring->socket);
 
   sc_start();
   return 0;
