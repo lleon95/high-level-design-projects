4794b090fef050799f7dcb2d256e9de1869eda42
eliecer9000 <eliecer9000@gmail.com>
2019-07-27 23:34:42 -0700

    TLM added to vga_decoder

---
+++ src/vga-decoder/vga_decoder.hpp
@@ -0,0 +1,88 @@
+//-----------------------------------------------------
+#include "systemc.h"
+#include "tlm.h"
+#include "tlm_utils/simple_initiator_socket.h"
+#include "tlm_utils/simple_target_socket.h"
+
+#define RESOLUTION 640*480
+#define PIXEL_SIZE 12
+#define PIXEL_POS 0
+#define H_SYNC_POS 12
+#define V_SYNC_POS 13
+#define GET_PIXEL(DATA) (DATA & 0xFFF)
+#define GET_H_SYNC(DATA) ((DATA & 1 << H_SYNC_POS) >> H_SYNC_POS)
+#define GET_V_SYNC(DATA) ((DATA & 1 << V_SYNC_POS) >> V_SYNC_POS)
+#define PIXEL_DELAY  39.722  //This is in nano secs
+#define PIXELS_IN_ROW 800
+#define ROWS_IN_FRAME 525
+#define ROW_DELAY PIXEL_DELAY * PIXELS_IN_ROW // This is in nano secs
+#define UPDATE_OUTPUT_DELAY 10 //nanoseconds
+#define SAMPLING_DELAY 10 //nanoseconds
+#define ADDRESSABLE_VIDEO_H_START 145
+#define ADDRESSABLE_VIDEO_H_END 784
+#define ADDRESSABLE_VIDEO_V_START 36
+#define ADDRESSABLE_VIDEO_V_END 515
+#define NOTIFY_FRAME_START_INTERRUPT_DELAY 10 //nanoseconds
+#define PACKAGE_LENGTH 2
+#define TRANSACTION_DELAY 10 //Nano seconds
+
+// Needed for the simple_target_socket
+#define SC_INCLUDE_DYNAMIC_PROCESSES
+
+struct vga_decoder : sc_module
+{
+    // TLM-2 socket, defaults to 32-bits wide, base protocol
+    tlm_utils::simple_initiator_socket<vga_decoder> initiator_socket;
+    tlm_utils::simple_target_socket<vga_decoder> target_socket;
+
+    //-----------Internal variables-------------------
+    int i;                //Internal counter to know the address we write.
+    short pixel_in;
+    short pixel;
+    bool previous_h_sync;
+    bool current_h_sync;
+    bool previous_v_sync;
+    bool current_v_sync;
+
+    int h_count, v_count;        // Count for the column and row.
+
+    sc_event count_column_event; //Event to notify the start of a new column
+    sc_event count_row_event;    //Event to notify the start of a new row
+    sc_event update_output_event;
+    sc_event decode_pixel_event;
+    sc_event sample_pixel_event;
+
+    void start_column_count();
+    void start_row_count();
+    void decode_pixel();
+    void column_count();
+    void row_count();
+    void update_output();
+    void sample_pixel();
+
+    // Function to receive transactions
+    virtual void b_transport(tlm::tlm_generic_payload& trans, sc_time& delay);
+
+    SC_CTOR(vga_decoder) : initiator_socket("socket") {
+
+        h_count  = 0;
+        v_count  = 0;
+        pixel_in = 0;
+        pixel    = 0;
+        i        = 0;
+
+        previous_h_sync = 1;
+        current_h_sync  = 1;
+        previous_v_sync = 1;
+        current_v_sync  = 1;
+
+        SC_THREAD(decode_pixel);
+        SC_THREAD(column_count);
+        SC_THREAD(row_count);
+        SC_THREAD(sample_pixel);
+        SC_THREAD(update_output);
+
+        target_socket.register_b_transport(this, &vga_decoder::b_transport);
+    } //End of Constructor
+}; // End of module vga_decoder
+
--- src/vga-decoder/vga_decoder_tbb.cpp
+++ src/vga-decoder/vga_decoder_tbb.cpp
@@ -1,59 +1,137 @@
 #include <systemc.h>
-#include "vga_decoder.cpp"
+#include "vga_decoder.hpp"
 
 #define ROWS_IN_SCREEN 525   // Rows in a screen, not the visible ones
-#define FRAMES 3             // Frames to simulate
+#define FRAMES 1             // Frames to simulate
 #define SIMULATION_TIME ROW_DELAY * ROWS_IN_SCREEN * FRAMES // In nano seconds
 #define H_SYNC_SYNCH_PULSE_LENGHT 96 //In pixels
 #define V_SYNC_SYNCH_PULSE_LENGHT 2  //In rows
+
+struct adc_simulator : sc_module {
+    
+    tlm_utils::simple_initiator_socket<adc_simulator> initiator_socket;
+
+    void send_data(short data)
+    {
+        tlm::tlm_generic_payload trans;
+        sc_time delay = sc_time(TRANSACTION_DELAY, SC_NS);
+
+        trans.set_command(tlm::TLM_WRITE_COMMAND);
+        trans.set_data_ptr(reinterpret_cast<unsigned char*>(&data));
+        trans.set_data_length(2);
+        trans.set_streaming_width(2); /* = data_length to indicate no streaming */
+        trans.set_byte_enable_ptr(0); /* 0 indicates unused */
+        trans.set_dmi_allowed(false); /* Mandatory initial value */
+        trans.set_response_status(
+            tlm::TLM_INCOMPLETE_RESPONSE ); /* Mandatory initial value */
+        cout << "ADC: Sending package: 0x" << hex << data << " @ "
+        << sc_time_stamp() << endl;
+        initiator_socket->b_transport(trans, delay);  // Blocking transport call
+    }
+
+    SC_CTOR(adc_simulator) : initiator_socket("initiator_socket"){} /* End of Constructor */
+}; /* End of Module adc_simulator */
+
+struct Memory: sc_module
+{
+    // TLM-2 socket, defaults to 32-bits wide, base protocol
+    tlm_utils::simple_target_socket<Memory> target_socket;
+
+    enum {SIZE = RESOLUTION};
+
+    SC_CTOR(Memory) : target_socket("socket")
+    {
+        // Register callback for incoming b_transport interface method call
+        target_socket.register_b_transport(this, &Memory::b_transport);
+
+        // Initialize memory with random data
+        for (int i = 0; i < SIZE; i++)
+            mem[i] = 0x0;
+    }
+
+    // TLM-2 blocking transport method
+    virtual void b_transport( tlm::tlm_generic_payload& trans, sc_time& delay )
+    {
+        tlm::tlm_command cmd = trans.get_command();
+        sc_dt::uint64    adr = trans.get_address();
+        short*             ptr = (short*) trans.get_data_ptr();
+        unsigned int     len = trans.get_data_length();
+        unsigned char*   byt = trans.get_byte_enable_ptr();
+        unsigned int     wid = trans.get_streaming_width();
+
+        // Obliged to check address range and check for unsupported features,
+        //   i.e. byte enables, streaming, and bursts
+        // Can ignore DMI hint and extensions
+        // Using the SystemC report handler is an acceptable way of signalling an error
+
+        if (adr >= sc_dt::uint64(SIZE) || byt != 0 || len > 4 || wid < len)
+            SC_REPORT_ERROR("TLM-2", "Target does not support given generic payload transaction");
+
+        // Obliged to implement read and write commands
+        if ( cmd == tlm::TLM_READ_COMMAND )
+            memcpy(ptr, &mem[adr], len);
+        else if ( cmd == tlm::TLM_WRITE_COMMAND ){
+            memcpy(&mem[adr], ptr, len);
+            cout << "Memory: 0x" << hex << *ptr << " was written to 0x"
+            << hex << adr << " @ " << sc_time_stamp() << endl;
+        }
+        // Obliged to set response status to indicate successful completion
+        trans.set_response_status( tlm::TLM_OK_RESPONSE );
+    }
+
+    short mem[SIZE];
+}; //End of Memory module
+
+
+SC_MODULE(Top)
+{
+    adc_simulator *adc;
+    vga_decoder   *decoder;
+    Memory        *memory;
+
+    SC_CTOR(Top)
+    {
+        // Instantiate components
+        adc     = new adc_simulator("ADC");
+        decoder = new vga_decoder("vga_decoder");
+        memory  = new Memory("memory");
+
+        adc->initiator_socket.bind(decoder->target_socket);
+        decoder->initiator_socket.bind(memory->target_socket);
+    }
+}; //End of Top module
 
 int
 sc_main (int argc, char* argv[])
 {
     srand (time(NULL));
-    // Inputs
-    sc_signal<sc_uint<PIXEL_SIZE> > pixel_in;
-    sc_signal<bool> hsync;
-    sc_signal<bool> vsync;
+
+    bool hsync;
+    bool vsync;
 
     //Outputs
-    sc_signal<sc_uint<PIXEL_SIZE> > pixel_out;
-    sc_signal<bool> frame_start;
+    // NONE
 
     // Instantiate the DUT
-    vga_decoder decoder("Decoder");
+    Top decoder("Decoder");
 
-    // Connect the DUT
-    decoder.pixel_in(pixel_in);
-    decoder.hsync(hsync);
-    decoder.vsync(vsync);
-    decoder.pixel_out(pixel_out);
-    decoder.frame_start(frame_start);
-
-    // Open VCD file
-    sc_trace_file *wf = sc_create_vcd_trace_file("vga_decoder");
-    wf->set_time_unit(1, SC_PS);
-
-    // Dump the desired signals to vcd
-    sc_trace(wf, pixel_in, "pixel_in");
-    sc_trace(wf, hsync, "h_sync");
-    sc_trace(wf, vsync, "v_sync");
-    sc_trace(wf, pixel_out, "pixel_out");
-    sc_trace(wf, frame_start, "frame_start");
-
-    // Simulation-control variables
+    // Simulation-internal variables
     int column = 1;
     int row = 1;
+    short data = 0;
 
     //Start the simulation
-    sc_start(0, SC_NS);
-    cout << "@" << sc_time_stamp() << endl;
+    sc_start(0,SC_NS);
+    cout << "@" << sc_time_stamp()<< endl;
 
     // Reset the inputs
     pixel_in = 0;
     hsync = 1;
     vsync = 1;
-    sc_start(1, SC_NS);
+    data = rand() % (1 << PIXEL_SIZE);
+    data = (short) (vsync << V_SYNC_POS | hsync << H_SYNC_POS | data);
+    decoder.adc->send_data(data);
+    sc_start(10,SC_NS);
 
     for (double simulated_time = 0; simulated_time < SIMULATION_TIME;
             simulated_time += PIXEL_DELAY) {
@@ -75,14 +153,14 @@ sc_main (int argc, char* argv[])
         if (row == ROWS_IN_FRAME + 1) { //It's a new frame
             row = 1;
         }
-
-        pixel_in.write(rand() % (1 << PIXEL_SIZE));
-
-        sc_start(PIXEL_DELAY, SC_NS);  // PIXEL_DELAY nano seconds elapsed
+        
+        data = rand() % (1 << PIXEL_SIZE);
+        data = (short) (vsync << V_SYNC_POS | hsync << H_SYNC_POS | data);
+        decoder.adc->send_data(data);
+        sc_start(PIXEL_DELAY,SC_NS);   // PIXEL_DELAY nano seconds elapsed
     }
-
-    cout << "@" << sc_time_stamp() << " Terminating simulation\n" << endl;
-    sc_close_vcd_trace_file(wf);
+    cout << "@" << sc_time_stamp() <<" Terminating simulation" << endl;
     return 0;// Terminate simulation
 
 }  //End of main
+
