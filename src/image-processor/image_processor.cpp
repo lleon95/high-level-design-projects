65b016777d48dc05a7420a1e5a55d20da7b96639
Emmanuel Madrigal <emma.madri97@gmail.com>
2019-07-28 10:58:54 -0600

    Modify image processor for interfacing with router
    A fix for the sobel at the start and end of buffer was applied

--- src/image-processor/image_processor.cpp
+++ src/image-processor/image_processor.cpp
@@ -16,12 +16,6 @@ const sc_int < CHANNEL_WIDTH + 1 > v_weights[3][3] = {
     { 1,  2,  1 }
 };
 
-/* Interrupt Handler */
-void
-image_processor::pixel_ready()
-{
-    _pixel_ready.notify(INTERRUPT_DELAY, SC_NS);
-}
 
 static sc_uint<CHANNEL_WIDTH>
 apply_sobel (sc_uint<CHANNEL_WIDTH> pixel_buffer[], int pixel_index)
@@ -33,21 +27,21 @@ apply_sobel (sc_uint<CHANNEL_WIDTH> pixel_buffer[], int pixel_index)
     /* Copy pixels to window buffer */
     for (int i = 0; i < 3; i++) {
         for( int j = 0; j < 3; j++) {
-            pixel_window[i][j] = pixel_buffer[(pixel_index + (i - 1) * WIDTH + (j - 1)) %
-                                              BUFFER_SIZE];
+	  int index = (pixel_index + (i - 1) * WIDTH + (j - 1));
+	  
+	  if(index > 0 && index < BUFFER_SIZE){
+            pixel_window[i][j] = pixel_buffer[index];
+	  }
+	  else{
+	    pixel_window[i][j] = 0;
+	  }
         }
     }
 
-    /* Get horizontal values */
+    /* Get vector values */
     for (int i = 0; i < 3; i++) {
         for( int j = 0; j < 3; j++) {
             h_value += h_weights[i][j] * pixel_window[i][j];
-        }
-    }
-
-    /* Get vertical values */
-    for (int i = 0; i < 3; i++) {
-        for( int j = 0; j < 3; j++) {
             v_value += v_weights[i][j] * pixel_window[i][j];
         }
     }
@@ -79,74 +73,42 @@ convert_to_grayscale (sc_uint<PIXEL_WIDTH> pixel)
 }
 
 void
-image_processor::return_pixel()
-{
-    tlm::tlm_generic_payload trans;
-    /* Pointer to be passed to the target, target is resposible for freeing it */
-    int* return_pixel = new int;
-    sc_time delay = sc_time(INTERRUPT_DELAY, SC_NS);
-
-    *return_pixel = (int)current_pixel;
-
-    trans.set_command( tlm::TLM_WRITE_COMMAND );
-    trans.set_data_ptr( reinterpret_cast<unsigned char*>(return_pixel) );
-    trans.set_data_length( 2 );
-    trans.set_streaming_width( 2 ); /* = data_length to indicate no streaming */
-    trans.set_byte_enable_ptr( 0 ); /* 0 indicates unused */
-    trans.set_dmi_allowed( false ); /* Mandatory initial value */
-    trans.set_response_status(
-        tlm::TLM_INCOMPLETE_RESPONSE ); /* Mandatory initial value */
-
-    initiator_socket->b_transport( trans, delay );  // Blocking transport call
-}
-
-void
-image_processor::process()
+image_processor::thread_process()
 {
     while(true) {
-        wait(_pixel_ready);
-        /* Convert pixel to gray and save to buffer */
-        pixel_buffer[pixel_index] = convert_to_grayscale( current_pixel );
+       wait(_pixel_ready);
 
-        /* Apply sobel */
-        gray = apply_sobel(pixel_buffer, pixel_index);
-
-        /* Get values to output pixel */
-        current_pixel.range(3 * CHANNEL_WIDTH - 1, 2 * CHANNEL_WIDTH) = gray;
-        current_pixel.range(2 * CHANNEL_WIDTH - 1, 1 * CHANNEL_WIDTH) = gray;
-        current_pixel.range(1 * CHANNEL_WIDTH - 1, 0) = gray;
-
-        /* Write value back*/
-        return_pixel();
-
-        pixel_index = (pixel_index + 1) % BUFFER_SIZE;
+        initiator->write(ENCODER_ADDRESS, (int)current_pixel, tlm::TLM_WRITE_COMMAND);
     }
 }
 
 void
-image_processor::b_transport(tlm::tlm_generic_payload& trans, sc_time& delay)
+image_processor::reading_process()
 {
-    tlm::tlm_command cmd = trans.get_command();
-    unsigned char*   ptr = trans.get_data_ptr();
-    unsigned int     len = trans.get_data_length();
-    unsigned char*   byt = trans.get_byte_enable_ptr();
-    unsigned int     wid = trans.get_streaming_width();
+    while(true) {
+        wait(*(incoming_notification));
+        bool command = target->command;
+        unsigned short data = target->incoming_buffer;
+        wait(sc_time(BUS_DELAY, SC_NS));
 
-    short* ptr_16_bits = reinterpret_cast<short*> (ptr);
+        if(command == tlm::TLM_WRITE_COMMAND) {
+            current_pixel = data & 0xFFF;
 
+            /* Convert pixel to gray and save to buffer */
+            pixel_buffer[pixel_index] = convert_to_grayscale( current_pixel );
 
-    if (byt != 0 || len > PACKAGE_LENGTH || wid < len) {
-        SC_REPORT_ERROR("TLM-2",
-                        "Target does not support given generic payload transaction");
+            /* Apply sobel */
+            gray = apply_sobel(pixel_buffer, pixel_index);
+
+            /* Get values to output pixel */
+            current_pixel.range(3 * CHANNEL_WIDTH - 1, 2 * CHANNEL_WIDTH) = gray;
+            current_pixel.range(2 * CHANNEL_WIDTH - 1, 1 * CHANNEL_WIDTH) = gray;
+            current_pixel.range(1 * CHANNEL_WIDTH - 1, 0) = gray;
+
+            /* Write value back*/
+            _pixel_ready.notify(INTERRUPT_DELAY, SC_NS);
+
+            pixel_index = (pixel_index + 1) % BUFFER_SIZE;
+        }
     }
-
-    /* Processor only accepts write operations */
-    if ( cmd == tlm::TLM_WRITE_COMMAND ) {
-        /* Copy pixels to internal buffer */
-        current_pixel = *ptr_16_bits & 0xFFF;
-
-        pixel_ready();
-    }
-
-    trans.set_response_status( tlm::TLM_OK_RESPONSE );
 }
--- src/image-processor/image_processor.hpp
+++ src/image-processor/image_processor.hpp
@@ -3,8 +3,7 @@
 
 #include "systemc.h"
 #include "tlm.h"
-#include "tlm_utils/simple_initiator_socket.h"
-#include "tlm_utils/simple_target_socket.h"
+#include "node.hpp"
 
 #define PIXEL_WIDTH 12
 #define CHANNEL_WIDTH 4
@@ -12,16 +11,14 @@
 #define INTERRUPT_DELAY 0
 
 #define WIDTH 640
+#define HEIGHT 480
 #define BUFFER_SIZE (WIDTH * 3)
 
 #define PACKAGE_LENGTH 2 /* 16 bits package length */
 
 
-struct image_processor : sc_module
-{
-    tlm_utils::simple_target_socket<image_processor> target_socket;
-    tlm_utils::simple_initiator_socket<image_processor> initiator_socket;
-   
+struct image_processor : Node
+{ 
     sc_event _pixel_ready;
 
     /* Local Variables */
@@ -33,21 +30,12 @@ struct image_processor : sc_module
     sc_uint<CHANNEL_WIDTH> pixel_buffer[BUFFER_SIZE];
     int pixel_index;
 
-    /* Interrupt Handler */
-    void pixel_ready();
+    void thread_process();
 
-    void process();
+    void reading_process();
 
-    void return_pixel();
 
-  /* Function to receive transfers  */
-  virtual void b_transport(tlm::tlm_generic_payload& trans, sc_time& delay);
-
-  SC_CTOR(image_processor) : target_socket("target_socket") {
-        SC_THREAD(process);
-	
-	target_socket.register_b_transport(this, &image_processor::b_transport);
-
+  image_processor(const sc_module_name & name) : Node(name) {
 	pixel_index = 0;
 
 	for(int i = 0; i < BUFFER_SIZE; i++) {
