b7c955db7d72d46351655aafdcea266291436681
Emmanuel Madrigal <emma.madri97@gmail.com>
2019-07-30 04:01:33 -0600

    Add adc and decoder to top module

--- src/top/top-tbb.cpp
+++ src/top/top-tbb.cpp
@@ -1,20 +1,22 @@
 #include "systemc.h"
 
+#include "adc.hpp"
 #include "dac.hpp"
 #include "image-processor.hpp"
 #include "node.hpp"
 #include "memory.hpp"
 #include "router.hpp"
+#include "vga_decoder.hpp"
 #include "vga-encoder.hpp"
 
 int
 sc_main (int argc, char* argv[])
 {
     /* Connect the DUT */
-    // Node* adc = new adc("adc");
-    // adc->addr = ADC_ADDRESS;
-    // Node* decoder = new decoder("decoder");
-    // decoder->addr = ENCODER_ADDRESS;
+    Node* adc = new analogic_digital_converter("adc");
+    adc->addr = ADC_ADDRESS;
+    Node* decoder = new vga_decoder("decoder");
+    decoder->addr = ENCODER_ADDRESS;
     Node* cpu =  new image_processor("SOBEL");
     cpu->addr = CPU_ADDRESS;
     Node* encoder = new vga_encoder("encoder");
@@ -22,17 +24,17 @@ sc_main (int argc, char* argv[])
     Node* dac =  new digital_analog_converter("DAC"); 
     dac->addr = DAC_ADDRESS;
 
-    // Router adc_router("adc-router", adc);
-    // Router decoder_router("decoder-router", decoder);
+    Router adc_router("adc-router", adc);
+    Router decoder_router("decoder-router", decoder);
     Router cpu_router("cpu-router", cpu);
     Router encoder_router("encoder-router", encoder);
     Router dac_router("dac-router", dac);
 
-    // adc_router.initiator_ring->socket.bind(decoder_router.target_ring->socket);
-    // decoder_router.initiator_ring->socket.bind(cpu_router.target_ring->socket);
+    adc_router.initiator_ring->socket.bind(decoder_router.target_ring->socket);
+    decoder_router.initiator_ring->socket.bind(cpu_router.target_ring->socket);
     cpu_router.initiator_ring->socket.bind(encoder_router.target_ring->socket);
     encoder_router.initiator_ring->socket.bind(cpu_router.target_ring->socket);
-    // dac_router.initiator_ring->socket.bind(adc_router.target_ring->socket);
+    dac_router.initiator_ring->socket.bind(adc_router.target_ring->socket);
     
     cout << "@" << sc_time_stamp() << " Terminating simulation\n" << endl;
     return 0;
