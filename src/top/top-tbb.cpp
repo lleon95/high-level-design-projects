c13121305a4d1c400730774c9141c9c3146dd2d6
Emmanuel Madrigal <emma.madri97@gmail.com>
2019-07-29 19:38:31 -0600

    Add connection for cpu encoder and dac

---
+++ src/top/top-tbb.cpp
@@ -0,0 +1,40 @@
+#include "systemc.h"
+
+#include "dac.hpp"
+#include "image-processor.hpp"
+#include "node.hpp"
+#include "memory.hpp"
+#include "router.hpp"
+#include "vga-encoder.hpp"
+
+int
+sc_main (int argc, char* argv[])
+{
+    /* Connect the DUT */
+    // Node* adc = new adc("adc");
+    // adc->addr = ADC_ADDRESS;
+    // Node* decoder = new decoder("decoder");
+    // decoder->addr = ENCODER_ADDRESS;
+    Node* cpu =  new image_processor("SOBEL");
+    cpu->addr = CPU_ADDRESS;
+    Node* encoder = new vga_encoder("encoder");
+    encoder->addr = ENCODER_ADDRESS;
+    Node* dac =  new digital_analog_converter("DAC"); 
+    dac->addr = DAC_ADDRESS;
+
+    // Router adc_router("adc-router", adc);
+    // Router decoder_router("decoder-router", decoder);
+    Router cpu_router("cpu-router", cpu);
+    Router encoder_router("encoder-router", encoder);
+    Router dac_router("dac-router", dac);
+
+    // adc_router.initiator_ring->socket.bind(decoder_router.target_ring->socket);
+    // decoder_router.initiator_ring->socket.bind(cpu_router.target_ring->socket);
+    cpu_router.initiator_ring->socket.bind(encoder_router.target_ring->socket);
+    encoder_router.initiator_ring->socket.bind(cpu_router.target_ring->socket);
+    // dac_router.initiator_ring->socket.bind(adc_router.target_ring->socket);
+    
+    cout << "@" << sc_time_stamp() << " Terminating simulation\n" << endl;
+    return 0;
+}
+
