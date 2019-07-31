02a4ead965876cd51a97e9371da30dfa61cb02e6
Emmanuel Madrigal <emmanuel.madrigal@ridgerun.com>
2019-07-31 13:32:12 -0600

    Added input rgb signals to the adc

--- src/adc/adc.cpp
+++ src/adc/adc.cpp
@@ -6,73 +6,17 @@
 void
 analogic_digital_converter::thread_process()
 {
-    srand (time(NULL));
+  while(true){
+    sc_uint<PIXEL_WIDTH> data = 0;
 
-    bool hsync;
-    bool vsync;
+    data.range(11, 8) = red_channel;
+    data.range(7, 4) = green_channel;
+    data.range(3, 0) = blue_channel;
 
-    // Control variables
-    int column = 1;
-    int row = 1;
-    sc_uint<PACKAGE_LENGH_IN_BITS> data = 0;
-
-    // Reset the inputs
-    hsync = 1;
-    vsync = 1;
-    data = rand() % MAX_PIXEL_VALUE_PLUS_ONE;
-    data.range(12, 12) = hsync;
-    data.range(13, 13) = vsync;
     cout << "ADC writing:\t" << data << " @ " << sc_time_stamp() << endl;
     initiator->write(DECODER_ADDRESS, (int)data, tlm::TLM_WRITE_COMMAND);
-    wait(sc_time(PIXEL_DELAY, SC_NS));
-#ifdef DEBUG //To be able to run the simulation fast    
-    for (int i = 0; i < DEBUG_PIXELS; i ++) {
-        if (column <= DEBUG_H_SYNC_SYNCH_PULSE_LENGTH) { //hsync should be cleared.
-            hsync = 0;
-        } else {
-            hsync = 1;
-        }
-        column++;
-
-        data = rand() % (MAX_PIXEL_VALUE_PLUS_ONE);
-        data.range(12, 12) = hsync;
-        data.range(13, 13) = vsync;
-
-        cout << "ADC writing:\t" << data << " @ " << sc_time_stamp() << endl;
-        initiator->write(DECODER_ADDRESS, (int)data, tlm::TLM_WRITE_COMMAND);
-        wait(sc_time(PIXEL_DELAY, SC_NS));
-    }
-#else
-    for (double simulated_time = 0; simulated_time < SIMULATION_TIME;
-            simulated_time += PIXEL_DELAY) {
-        if (column <= H_SYNC_SYNCH_PULSE_LENGHT) { //hsync should be cleared.
-            hsync = 0;
-        } else {
-            hsync = 1;
-        }
-        if (row <= V_SYNC_SYNCH_PULSE_LENGHT) { //vsync should be cleared.
-            vsync = 0;
-        } else {
-            vsync = 1;
-        }
-        column++;
-        if (column == PIXELS_IN_ROW + 1) { //It's a new row
-            column = 1;
-            row++;
-        }
-        if (row == ROWS_IN_FRAME + 1) { //It's a new frame
-            row = 1;
-        }
-
-        data = rand() % (MAX_PIXEL_VALUE_PLUS_ONE);
-        data.range(12, 12) = hsync;
-        data.range(13, 13) = vsync;
-
-        cout << "ADC writing:\t" << data << " @ " << sc_time_stamp() << endl;
-        initiator->write(DECODER_ADDRESS, (int)data, tlm::TLM_WRITE_COMMAND);
-        wait(sc_time(PIXEL_DELAY, SC_NS));   // PIXEL_DELAY nano seconds elapsed
-    }
-#endif
+    wait(sc_time(PIXEL_DELAY, SC_NS));   // PIXEL_DELAY nano seconds elapsed
+  }
 }
 
 void
